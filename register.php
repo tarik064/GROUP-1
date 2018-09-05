09891eeb86302fcfacc083fe1f82a5ab9dae4aa7
Shahriar Kabir Shourav <35093508+shahriar143014041@users.noreply.github.com>
2018-09-06 05:45:46 +0600

    Add files via upload

---
+++ register.php
@@ -0,0 +1,54 @@
+<?php
+	
+
+	include 'includes/session.php';
+
+	if(isset($_POST['signup'])){
+		$firstname = $_POST['firstname'];
+		$lastname = $_POST['lastname'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$repassword = $_POST['repassword'];
+
+		$_SESSION['firstname'] = $firstname;
+		$_SESSION['lastname'] = $lastname;
+		$_SESSION['email'] = $email;
+
+		
+
+		if($password != $repassword){
+			$_SESSION['error'] = 'Passwords did not match';
+			header('location: signup.php');
+		}
+		else{
+			$conn = $pdo->open();
+
+			$stmt = $conn->prepare("SELECT COUNT(*) AS numrows FROM users WHERE email=:email");
+			$stmt->execute(['email'=>$email]);
+			$row = $stmt->fetch();
+			if($row['numrows'] > 0){
+				$_SESSION['error'] = 'Email already taken';
+				header('location: signup.php');
+			}
+			else{
+				$now = date('Y-m-d');
+				$password = password_hash($password, PASSWORD_DEFAULT);
+
+				//generate code
+				$set='123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+				$code=substr(str_shuffle($set), 0, 12);
+
+				
+				$pdo->close();
+
+			}
+
+		}
+
+	}
+	else{
+		$_SESSION['error'] = 'Fill up signup form first';
+		header('location: signup.php');
+	}
+
+?>
