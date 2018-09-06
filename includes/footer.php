e3cfb637a9da642abba9d9e21cf2abdac5a45ae7
tarik064 <40644157+tarik064@users.noreply.github.com>
2018-09-06 10:00:42 +0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+
+    <div class="b-footer__container">
+
+<div class=" b-footer__links">
+
+<div class="b-footer__list">
+<ul>
+<p class="b-footer__column-title">Service</p>
+<li>
+<a href="">Browse services</a>
+</li>
+<li>
+<a  href="">Sign up</a>
+</li>
+<li>
+<a href="">Log in</a>
+</li>
+<li>
+<a target="_blank" href="">Privacy policy</a>
+</li>
+<li>
+<a data-reveal-id="feedback-modal" href="">Contact us</a>
+</li>
+</ul>
+</div>
+
+</div>
+
+<div class="b-footer__cta">
+
+<a class="button expand m-button-outline-alt" href="">View all services</a>
+</div>
+
+</div>
+</div>
